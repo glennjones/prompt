fa06e563443bf850d45169e411822092a21f34cd
Glenn Jones <glennjonesnet@gmail.com>
2023-03-15 15:06:24 +0000

    fix: restate index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { OpenAI } from './src/models/openai.js';
-//import { Prompter } from './src/prompts/prompter.js';
+import { Prompter } from './src/prompts/prompter.js';
 
-export default { OpenAI };
+export default { OpenAI, Prompter };

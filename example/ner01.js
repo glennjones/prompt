1c7b8f70867bfe4446ee7c08424869650affe0eb
Glenn Jones <glennjonesnet@gmail.com>
2023-03-29 23:11:41 +0100

    chore: update example code

--- example/ner01.js
+++ example/ner01.js
@@ -8,20 +8,20 @@ const {OpenAI, Prompter} = require('../index.js');
 
 async function test() {
   let model = new OpenAI(apiKey);
-  let nlpPrompter = new Prompter(model);
+  let prompt = new Prompter(model);
 
   let text = "UX/UI Designer, Edinburgh or fully Remote"
   let labels = [
-    { label: 'JOBTITLE', text: 'UX/UI Designer', start: 0, end: 13 },
-    { label: 'LOCATION', text: 'Edinburgh', start: 16, end: 25 },
-    { label: 'REMOTE_OPTION', text: 'fully Remote', start: 28, end: 40 },
+    { label: 'JOBTITLE', text: 'UX/UI Designer' },
+    { label: 'LOCATION', text: 'Edinburgh'  },
+    { label: 'REMOTE_OPTION', text: 'fully Remote' },
     { branch: 'UX', group: 'Design' }
   ]
   let fineTune = [
     [text, labels]
   ]
   
-  let result = await nlpPrompter.fit('ner.njk', {
+  let result = await prompt.fit('ner', {
       domain: 'ux recruitment',
       labels: ["JOBTITLE", "EMPLOYMENT_TYPE", "DURATION", "LOCATION", "REMOTE_OPTION"],
       textInput: 'UX/UI Copy Writer, Edinburgh or Hybrid',
